6908ffb46527cc8da67e1b620b1f28d873928d1f
Joseph Tatum <josephtatum@gmail.com>
2019-12-02 15:11:45 -0800

    created console logs for index.js and changed isObject to return a boolean

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-const { isNumber } = require('./lib/types.js');
+const {
+  isNumber,
+  isString,
+  isBoolean,
+  isArray,
+  isObject,
+  isFunction,
+  castToNumber,
+  castToString,
+  castToBoolean
+} = require('./lib/types.js');
 
-console.log(isNumber('3'));
+console.log(isNumber(3));
+console.log(isString('3'));
+console.log(isArray([1, 2, 3]));
+console.log(isBoolean(true));
+console.log(isObject({ name: 'doug' }));
+console.log(isFunction(isNumber));
+console.log(castToNumber('3'));
+console.log(castToString(3));
+console.log(castToBoolean(3));
